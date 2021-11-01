a65d2544303f40a03d10d95177e3bea6e537ca19
evazhengg <eva.zheng23@sitechhs.com>
2021-11-01 12:21:14 -0400

    fixed jpgs

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -8,3 +8,6 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+//Add a new boolean data property `onSale` and create a computed property that takes `brand`, `product` and `onSale` and prints out a string whenever `onSale` is true.
+
