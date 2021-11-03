235681dff6787c4f74c770e300e3a8a3815a9984
evazhengg <eva.zheng23@sitechhs.com>
2021-11-02 21:56:30 -0400

    ?

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,53 +1,57 @@
 <template>
- <div id="app">
-   <nav class="navbar"> </nav>
+<div id="app">
+  <nav class="navbar"> </nav>
 
 <section id="user-choices">
+
+
+     <div id="user-selections">
+     <div id="cake-selections">
+      <div class = "selections" v-for="cake in cakeChoices" :key="cake">
+	 <button @click="updateOrder()"> {{ cake }} </button>
+      </div>
+  </div>
   
-
-      <div id="user-selections">
-      <div id="cake-selections">
-       <div class = "selections" v-for="cake in cakeChoices" :key="cake"> </div> 
-    <button> @click="updateCakes(cake)"> {{ cake }} </button>
-
 <div id="drink-selections">
-<div class = "selections" v-for="drink in drinkChoices" :key="drink"> </div>
-<button> @click="updatedrinks (Drink)"> {{ drink }} </button>
-      </div>
-      </div>
+<div class = "selections" v-for="drink in latteChoices" :key="drink"> 
+	<button @click="updateOrder()"> {{ drink }} </button>
+</div>
+     </div>
+   
 
 
 </section>
 
-<section id="order-total"> 
+ <section id="order-total">
 <h2> Amount in Cart </h2>
-<li class="info" v-for="order in order" :key="order">{{ ordered }}</li>  
+<p> {{ subtotal  }} </p>
 
-  </section>
+ </section>
 </template>
 
 <script>
 
 export default {
- 	data(){
-      return {
-        cakeChoices: [
-          {
-          typeName: "Chocolate",
-          typeImage: ("./assets/Chocolate-cake.jpg"),
- 				},
-        {
-        typeName: "Strawberry",
-        typeImage: ("./assets/strawberry-cake.jpg"),
-        },
-        {
-        typeName: "Vanilla",
-        typeImage: ("./assets/vanilla-cake.jpg"),
-        },
-        {
-        typeName: "Matcha",
-        typeImage: ("./assets/matcha-cake.jpg"),
-        },
+ data(){
+     return {
+        subtotal: 0,
+       cakeChoices: [
+         {
+         typeName: "Chocolate",
+         typeImage: ("./assets/Chocolate-cake.jpg"),
+       },
+       {
+       typeName: "Strawberry",
+       typeImage: ("./assets/strawberry-cake.jpg"),
+       },
+       {
+       typeName: "Vanilla",
+       typeImage: ("./assets/vanilla-cake.jpg"),
+       },
+       {
+       typeName: "Matcha",
+       typeImage: ("./assets/matcha-cake.jpg"),
+       },
 ],
 latteChoices: [
 {
@@ -65,28 +69,31 @@ flavorImage: ("./assets/banana.jpg"),
 {
 flavorName: "Popcorn",
 flavorImage: ("./assets/popcorn.jpg"),
-  },
+ },
 ],
-
-
 }
-  },
-
-
 }
 
-</script> 
+methods: {
+	updateOrder() {
+		this.subtotal = subtotal++
+}
 
-<style lang ="css"> 
-.sections {
-  display:flex;
+}
+
+</script>
+
+<style lang ="css"
+
+.choice { 
+  display:flex; 
   flex-direction: column;
-  align-items: center;
-  width: 50%;
-}
- .image {
-  width: 7rem;
-  height: 7rem; 
 }
 
-</style>
+#cakeChoices,
+#latteChoices {
+  display:flex;
+  flex-direction: row; 
+}
+}
+}
